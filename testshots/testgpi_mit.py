35fb365732e344de32b180dd5c9908f6ad5b2a67
harryh5427 <harryh5427@gmail.com>
2018-03-06 13:06:10 -0500

    testgpi_mit.py inits DIO2 successfully

--- testshots/testgpi_mit.py
+++ testshots/testgpi_mit.py
@@ -9,15 +9,12 @@ Harry Han, Feb 27, 2018
 """
 from MDSplus import *
 from MitDevices.acq132 import ACQ132
-from RfxDevices import DIO2_ENCDEC
 import sys
 
 s=int(sys.argv[1])
 myTree=Tree("spectroscopy",-1)
 myTree.createPulse(s) #Copies the model tree
 myTree=Tree("spectroscopy",s)
-dio2=DIO2_ENCDEC(myTree.getNode("gpi.apd_array.hardware.DIO2"))
+myDIO2=myTree.getNode("GPI.APD_ARRAY.HARDWARE:DIO2")
 
-dio2.init()
-
-
+myTree.tcl('do /meth '+myDIO2.getFullPath()+' init')
